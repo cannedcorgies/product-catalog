d123272f95d9b63b5339b2767ff3265c7b2bef94
cannedcorgies <artistafdo@gmail.com>
2025-05-19 14:50:24 -0700

    fetching implemented

--- src/pages/ProductCatalog.jsx
+++ src/pages/ProductCatalog.jsx
@@ -5,58 +5,36 @@
 //  - minimal descriptions
 //  - it's all about readability
 //  - avoid oversaturation
-
-import React from "react";
-import ProductCard from "../components/ProductCard";
-import testImage from "../assets/images/kitty.jpg";
-
-const mockProducts = [
-  {
-    id: 1,
-    name: "Wireless Headphones",
-    price: "99.99",
-    image: testImage,
-  },
-  {
-    id: 2,
-    name: "Smart Watch",
-    price: "199.99",
-    image: testImage,
-  },
-  {
-    id: 3,
-    name: "Portable Speaker",
-    price: "59.99",
-    image: testImage,
-  },
-  {
-    id: 4,
-    name: "Gaming Mouse",
-    price: "49.99",
-    image: testImage,
-  },
-  {
-    id: 5,
-    name: "Gaming Mouse",
-    price: "49.99",
-    image: testImage,
-  },
-
-];
-
+//
 // fern vocab:
 //  - grid          we're gonna be working with a grid
 //  - grid-cols-1   columns on mobile
 //  - sm:/md/xl:grid-cols...        number of columns on different screen sizes
 //  - gap-6         spacing between items
 
+import React, { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
+import testImage from "../assets/images/kitty.jpg";
+
 const ProductCatalog = () => {
 
+    /////POPULATE GRID
+    const [products, setProducts] = useState([]);       // empty products array
+
+    // when the component shows up...
+    useEffect(() => {
+        fetch("https://cart-api.alexrodriguez.workers.dev/products")                // fetch from url...
+        .then((response) => response.json())                                        // store data as a json...
+        .then((data) => setProducts(data))                                          // updates...
+        .catch((error) => console.error("ERROR FETCHING THE PRODUCTS:", error));    // ERROR CATCHER
+    }, []);
+
+    /////GENERATE THE GRID
     return (
         <div className="p-6">
-            <h1 className="text-2x1 font-semibold mb-6">Our Products</h1>
+            <h1 className="text-2x1 font-semibold mb-6">Digital Nest Store</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
-                {mockProducts.map((product) => (
+                {products.map((product) => (
                     <ProductCard
                     key={product.id}
                     name={product.name}
