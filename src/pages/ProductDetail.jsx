84762abe0942f5c04903de6014d84456736d2785
cannedcorgies <artistafdo@gmail.com>
2025-05-20 14:04:17 -0700

    ===PARTS 02 & 03 COMPLETE: PRODUCT PAGE AND CART DROPDOWN===

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -11,7 +11,7 @@
 
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { useCart } from "../context/CartContext";
+import { useCart } from "../context/CartContext"; 
 
 const ProductDetail = () => {
     const { id } = useParams();
